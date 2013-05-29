d0411869ef159aa24fd1a1b908d75c5b696d39ba
Michael John Burgess <michael@mjburgess.co.uk>
2013-05-29 18:52:14 +0100

    switched PHPUnit bootstrap to composer

--- test/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-set_include_path(get_include_path() . PATH_SEPARATOR . dirname(__DIR__) . DIRECTORY_SEPARATOR . 'lib');
-
-spl_autoload_register(function ($className) {
-    if(substr($className, 0, 7) == 'PHPUnit') {return true;}
-    require str_replace('\\', DIRECTORY_SEPARATOR, ltrim($className, '\\')) . '.php';
-});
